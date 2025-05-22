807284631d0b851ec2417c382d945e1d8920cd5d
ealbanca <ealbanca@byui.edu>
2025-05-22 17:05:01 -0600

    created todo-list, h2 and entry on js

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 // Create container div
 const container = document.createElement('div');
 container.className = 'container';
+
+// Create todo-list div
+const todoList = document.createElement('div');
+todoList.className = 'todo-list';
+
+// Create and append, added h2
+const heading = document.createElement('h2');
+heading.textContent = 'To Do List';
+todoList.appendChild(heading);
+
+// Create entry div
+const entry = document.createElement('div');
+entry.className = 'entry';
