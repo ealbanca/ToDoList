699628679de52376a0c670b1a5da8ebaf3871416
ealbanca <ealbanca@byui.edu>
2025-05-23 21:04:23 -0600

    Added comments to the script.js files

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,13 @@
+// Imported all needed functions from other js files
 import { removeTask } from "./removeTask.js";
 import { addTask } from "./addTask.js";
 import { saveList, loadList } from "./storage.js";
 
-// Create container div
+// Create container div, this will hold the todo-list
 const container = document.createElement('div');
 container.className = 'container';
 
-// Create todo-list div
+// Create todo-list div, this will hold the heading, name, instructions, entry and task list
 const todoList = document.createElement('div');
 todoList.className = 'todo-list';
 
@@ -14,19 +15,19 @@ todoList.className = 'todo-list';
 const heading = document.createElement('h2');
 heading.textContent = 'To Do List';
 
-// My name
+// Added my name
 const name = document.createElement('h3');
 name.textContent = 'By: Hared Albancando Robles';
 
-//Added Instructions
+//Added Instructions at the top of the list
 const instructions = document.createElement('p');
 instructions.textContent = 'Please enter a new task. If you have already enter tasks, you can click on a task to mark it as completed, or click the "X" to remove a task. Remember to keep your list always updated.';
 
-//Create entry div
+//Create entry div ( will hold the input field and button)
 const entry = document.createElement('div');
 entry.className = 'entry';
 
-// Create input field
+// Create input field (to enter new tasks)
 const input = document.createElement('input');
 input.type = 'text';
 input.id = 'inputBox';
@@ -58,15 +59,17 @@ container.appendChild(todoList);
 //Append container to the body
 document.body.appendChild(container);
 
+//Load the list from local storage
 loadList(taskList);
 
+// Add event listener to the button to add entered tasks by user
 button.addEventListener('click', function () {
     addTask(input, taskList);
     saveList(taskList);
 
 });
 
-// Function to remove tasks from the todo list
+// Function to remove existing tasks from the todo list
 todoList.addEventListener('click', function (e) {
     if (e.target.tagName === 'LI') {
         e.target.classList.toggle('completed');
